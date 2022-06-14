462fe2804d60e921f6c13a9bc29709db258ecb51
aci2n <alvarocalace@hotmail.com>
2022-06-14 11:21:26 -0300

    play card

--- src/libtruco.c
+++ src/libtruco.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdbool.h>
+#include <assert.h>
 
 #include "libtruco.h"
 #include "card.c"
@@ -11,14 +12,23 @@
 #define TRUCO_HAND_SIZE 3
 #define TRUCO_MAX_CHOICES 128
 
+typedef struct truco_lap truco_lap;
 typedef struct truco_round truco_round;
 typedef struct truco_choices truco_choices;
+
+struct truco_lap {
+  size_t size;
+  truco_card strongest_card[TRUCO_MAX_TEAMS];
+  truco_card stack[TRUCO_MAX_PLAYERS];
+};
 
 struct truco_round {
   size_t score[TRUCO_MAX_TEAMS];
   truco_playable_card hands[TRUCO_MAX_PLAYERS][TRUCO_HAND_SIZE];
-  size_t stack_size;
-  truco_card stack[TRUCO_MAX_PLAYERS * TRUCO_HAND_SIZE]; 
+  size_t current_lap;
+  truco_lap laps[TRUCO_HAND_SIZE];
+  bool drew_first;
+  size_t bonus;
 };
 
 struct truco_choices {
@@ -76,6 +86,61 @@ void truco_advance_round(truco_state* state) {
   truco_start_round(state);
 }
 
+static
+inline
+size_t truco_get_team(size_t player) {
+  return player % 2;
+}
+
+static
+void truco_compute_round_end(truco_state* state) {
+  truco_round* round = &(state->round);
+
+  if (round->score[0] > round->score[1]) {
+    state->score[0] += round->bonus;
+  } else if (round->score[1] > round->score[0]) {
+    state->score[1] += round->bonus;
+  }
+}
+
+static
+void truco_play_card(truco_state* state, size_t i) {
+  truco_round* round = &(state->round);
+  truco_lap* lap = &(round->laps[round->current_lap]);
+  truco_playable_card* playable = &(round->hands[state->current_player][i]);
+  size_t team = truco_get_team(state->current_player);
+
+  assert(!playable->played);
+  
+  if (truco_cards[playable->card].power > truco_cards[lap->strongest_card[team]].power) {
+    lap->strongest_card[team] = playable->card;
+  }
+
+  playable->played = true;
+  lap->stack[lap->size++] = playable->card;
+
+  if (lap->size == state->players) {
+    size_t strongest_0 = truco_cards[lap->strongest_card[0]].power;
+    size_t strongest_1 = truco_cards[lap->strongest_card[1]].power;
+
+    if (strongest_0 > strongest_1) {
+      round->score[0]++;
+    } else if (strongest_1 > strongest_0) {
+      round->score[1]++;
+    } else if (round->current_lap == 0) {
+      round->drew_first = true;
+    }
+
+    size_t diff = abs((int)(round->score[0] - round->score[1]));
+
+    if ((round->drew_first && diff > 0) || diff > 1 || round->current_lap == 2) {
+      truco_compute_round_end(state);
+    } else {
+      round->current_lap++;
+    }
+  }
+}
+
 void truco_dump(truco_state* state) {
   printf("{\n");
   printf(" Players: %lu\n", state->players);
@@ -86,6 +151,8 @@ void truco_dump(truco_state* state) {
   truco_round round = state->round;
 
   printf("  Score: %lu - %lu\n", round.score[0], round.score[1]);
+  printf("  Bonus: %lu\n", round.bonus);
+  printf("  Drew first: %u\n", round.drew_first);
 
   for (size_t i = 0; i < state->players; i++) {
     truco_playable_card* hand = round.hands[i];
@@ -100,10 +167,15 @@ void truco_dump(truco_state* state) {
     printf("]\n");
   }
 
-  printf("  Stack: [ ");
-  for (size_t i = 0; i < round.stack_size; i++) {
-    truco_card_def def = truco_cards[round.stack[i]];
-    printf("%s ", def.name);
+  printf("  Laps: [ ");
+  for (size_t i = 0; i < round.current_lap; i++) {
+    truco_lap* lap = &(round.laps[i]);
+    printf("   {\n");
+    printf("    Size: %lu\n", lap->size);
+    printf("    Strongest card: %s - %s\n", 
+        truco_cards[lap->strongest_card[0]].name,
+        truco_cards[lap->strongest_card[1]].name);
+    printf("   }\n");
   }
   printf("]\n");
 
@@ -123,6 +195,12 @@ void truco_dispatch(truco_state* state, truco_command const command) {
 
     case advance_round:
       truco_advance_round(state);
+      break;
+
+    case play_first:
+    case play_second:
+    case play_third:
+      truco_play_card(state, command - play_first);
       break;
   }
 }
--- test/libtruco_test.c
+++ test/libtruco_test.c
@@ -7,11 +7,9 @@ int main(int const argc, char const*const argv[static argc + 1]) {
   truco_state* state = malloc(sizeof(truco_state));
   printf("sizeof(truco_state): %lu\n", sizeof(*state));
 
-  truco_dispatch(state, start_game_two);
-  truco_dump(state);
-
-  truco_dispatch(state, advance_round);
-  truco_dump(state);
+  truco_dispatch(state, start_game_two); truco_dump(state);
+  truco_dispatch(state, advance_round); truco_dump(state);
+  truco_dispatch(state, play_first); truco_dump(state);
 
   return EXIT_SUCCESS;
 }
